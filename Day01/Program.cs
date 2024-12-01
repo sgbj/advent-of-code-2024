e676b980aec9735305228c2ecd2dd55039a083ab
sgbj <scott@batary.dev>
2024-12-01 16:06:31 -0600

    day 1 ❄️

---
+++ Day01/Program.cs
@@ -0,0 +1,14 @@
+﻿var lines = await File.ReadAllLinesAsync("input.txt");
+
+var values = lines.Select(line => line.Split("   ").Select(int.Parse).ToArray()).ToList();
+
+var list1 = values.Select(value => value[0]).Order().ToList();
+var list2 = values.Select(value => value[1]).Order().ToList();
+
+var totalDistance = list1.Zip(list2, (value1, value2) => Math.Abs(value1 - value2)).Sum();
+
+Console.WriteLine(totalDistance);
+
+var similarityScore = list1.Sum(value1 => value1 * list2.Count(value2 => value2 == value1));
+
+Console.WriteLine(similarityScore);
