e85385b933dcb9cd78aff09e9a191d4ff8a70224
sgbj <scott@batary.dev>
2024-12-02 20:05:16 -0600

    day 2 📈

---
+++ Day02/Program.cs
@@ -0,0 +1,21 @@
+﻿var reports = (await File.ReadAllLinesAsync("input.txt"))
+    .Select(report => report.Split(" ").Select(int.Parse).ToArray())
+    .ToList();
+
+var count1 = reports.Count(IsSafe);
+
+Console.WriteLine(count1);
+
+var count2 = reports.Count(IsSafeWithProblemDampener);
+
+Console.WriteLine(count2);
+
+bool IsSafeWithProblemDampener(int[] levels) => 
+    IsSafe(levels) || levels.Where((_, i) => IsSafe([..levels[..i], ..levels[(i + 1)..]])).Any();
+
+bool IsSafe(int[] levels)
+{
+    var diffs = levels.Zip(levels.Skip(1), (a, b) => b - a).ToArray();
+    var sign = Math.Sign(diffs.First());
+    return diffs.All(diff => sign == Math.Sign(diff) && Math.Abs(diff) is >= 1 and <= 3);
+}
