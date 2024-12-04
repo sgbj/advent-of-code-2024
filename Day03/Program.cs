92bd370e5082ab14315cb724df45981d1c7c71c8
sgbj <scott@batary.dev>
2024-12-03 20:16:41 -0600

    day 3 ☃️

---
+++ Day03/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+var input = await File.ReadAllTextAsync("input.txt");
+
+var answer1 = Regex.Matches(input, @"mul\((\d+),(\d+)\)")
+    .Sum(match => int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value));
+
+Console.WriteLine(answer1);
+
+var (_, answer2) = Regex.Matches(input, @"do\(\)|don't\(\)|mul\((\d+),(\d+)\)")
+    .Aggregate((mulEnabled: true, total: 0), (state, match) => match.Value switch
+    {
+        "do()" => (true, state.total),
+        "don't()" => (false, state.total),
+        _ => state.mulEnabled ? (true, state.total + int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value)) : state
+    });
+
+Console.WriteLine(answer2);
