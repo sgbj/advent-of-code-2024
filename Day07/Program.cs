f6c7d3afc47c9dda832f9fc6dcfe027a0c714d4b
sgbj <scott@batary.dev>
2024-12-06 23:47:35 -0600

    day 7 🧮

---
+++ Day07/Program.cs
@@ -0,0 +1,26 @@
+﻿var input = await File.ReadAllLinesAsync("input.txt");
+
+var equations = input.Select(line => line.Split(": "))
+    .Select(split => (Result: long.Parse(split[0]), Operands: split[1].Split(' ').Select(long.Parse).ToArray()))
+    .ToList();
+
+var answer1 = equations.Where(eq => IsSolvable(eq.Result, eq.Operands, ["+", "*"], eq.Operands[0]))
+    .Sum(eq => eq.Result);
+
+Console.WriteLine(answer1);
+
+var answer2 = equations.Where(eq => IsSolvable(eq.Result, eq.Operands, ["+", "*", "||"], eq.Operands[0]))
+    .Sum(eq => eq.Result);
+
+Console.WriteLine(answer2);
+
+bool IsSolvable(long result, long[] operands, string[] operators, long current, int index = 1) =>
+    index == operands.Length
+        ? current == result
+        : operators.Any(op => IsSolvable(result, operands, operators, op switch
+        {
+            "+" => current + operands[index],
+            "*" => current * operands[index],
+            "||" => long.Parse($"{current}{operands[index]}"),
+            _ => throw new InvalidOperationException()
+        }, index + 1));
