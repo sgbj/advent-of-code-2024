9a8e220b9750b0d35fabb881f7545e164a2a7270
sgbj <scott@batary.dev>
2024-12-08 01:17:51 -0600

    day 8 📡

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -1,3 +1,51 @@
-﻿var input = await File.ReadAllTextAsync("input.txt");
+﻿var input = await File.ReadAllLinesAsync("input.txt");
+var antennas = input.SelectMany((row, y) => row.Select((c, x) => (c, x, y)))
+    .Where(a => a.c != '.')
+    .GroupBy(a => a.c)
+    .ToList();
 
-Console.WriteLine(input);
+var antinodes1 = new HashSet<(int X, int Y)>();
+var antinodes2 = new HashSet<(int X, int Y)>();
+
+foreach (var frequency in antennas)
+{
+    foreach (var a1 in frequency)
+    {
+        foreach (var a2 in frequency)
+        {
+            if (a1 == a2)
+            {
+                continue;
+            }
+
+            if (InBounds(a1.x + (a2.x - a1.x) * 2, a1.y + (a2.y - a1.y) * 2))
+            {
+                antinodes1.Add((a1.x + (a2.x - a1.x) * 2, a1.y + (a2.y - a1.y) * 2));
+            }
+
+            var (dx, dy) = Normalize(a2.x - a1.x, a2.y - a1.y);
+
+            for (var step = 0; InBounds(a1.x + dx * step, a1.y + dy * step); step--)
+            {
+                antinodes2.Add((a1.x + dx * step, a1.y + dy * step));
+            }
+
+            for (var step = 0; InBounds(a1.x + dx * step, a1.y + dy * step); step++)
+            {
+                antinodes2.Add((a1.x + dx * step, a1.y + dy * step));
+            }
+        }
+    }
+}
+
+Console.WriteLine(antinodes1.Count);
+Console.WriteLine(antinodes2.Count);
+
+bool InBounds(int x, int y) => y >= 0 && y < input.Length && x >= 0 && x < input[y].Length;
+
+(int X, int Y) Normalize(int x, int y)
+{
+    var gcd = Gcd(Math.Abs(x), Math.Abs(y));
+    return (x / gcd, y / gcd);
+    int Gcd(int a, int b) => b == 0 ? a : Gcd(b, a % b);
+}
