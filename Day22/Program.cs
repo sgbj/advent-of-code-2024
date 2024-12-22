33f133a5a95b33f5da3d653adb76235de3ce769c
sgbj <scott@batary.dev>
2024-12-21 23:44:52 -0600

    day 22 part 1 🐒

---
+++ Day22/Program.cs
@@ -0,0 +1,22 @@
+﻿var initialSecretNumbers = (await File.ReadAllLinesAsync("input.txt")).Select(long.Parse).ToArray();
+
+var sum = 0L;
+
+foreach (var initialSecretNumber in initialSecretNumbers)
+{
+    var secretNumber = initialSecretNumber;
+
+    for (var i = 0; i < 2000; i++)
+    {
+        secretNumber ^= secretNumber * 64;
+        secretNumber %= 16777216;
+        secretNumber ^= secretNumber / 32;
+        secretNumber %= 16777216;
+        secretNumber ^= secretNumber * 2048;
+        secretNumber %= 16777216;
+    }
+
+    sum += secretNumber;
+}
+
+Console.WriteLine(sum);
