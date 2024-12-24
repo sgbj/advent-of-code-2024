b3568d76ba2c02e79ed6089685f35bb05c806296
sgbj <scott@batary.dev>
2024-12-24 13:01:55 -0600

    days 22 part 2 and 24 part 1

--- Day22/Program.cs
+++ Day22/Program.cs
@@ -1,11 +1,26 @@
 ﻿var initialSecretNumbers = (await File.ReadAllLinesAsync("input.txt")).Select(long.Parse).ToArray();
 
-var sum = 0L;
+Console.WriteLine(initialSecretNumbers.Sum(initialSecretNumber => GenerateSecretNumbers(initialSecretNumber).Last()));
 
-foreach (var initialSecretNumber in initialSecretNumbers)
+var sequences = new Dictionary<(string Sequence, int Monkey), long>();
+
+for (var monkey = 0; monkey < initialSecretNumbers.Length; monkey++)
 {
-    var secretNumber = initialSecretNumber;
+    var secretNumbers = GenerateSecretNumbers(initialSecretNumbers[monkey]).ToArray();
+    var bananas = secretNumbers.Select(secretNumber => secretNumber % 10).ToArray();
+    var changes = bananas.Zip(bananas.Skip(1), (a, b) => b - a).ToArray();
 
+    for (var i = 4; i < secretNumbers.Length; i++)
+    {
+        var sequence = string.Join(",", changes[(i - 4)..i]);
+        sequences.TryAdd((sequence, monkey), bananas[i]);
+    }
+}
+
+Console.WriteLine(sequences.GroupBy(x => x.Key.Sequence).Select(g => g.Sum(x => x.Value)).Max());
+
+static IEnumerable<long> GenerateSecretNumbers(long secretNumber)
+{
     for (var i = 0; i < 2000; i++)
     {
         secretNumber ^= secretNumber * 64;
@@ -14,9 +29,6 @@ foreach (var initialSecretNumber in initialSecretNumbers)
         secretNumber %= 16777216;
         secretNumber ^= secretNumber * 2048;
         secretNumber %= 16777216;
+        yield return secretNumber;
     }
-
-    sum += secretNumber;
 }
-
-Console.WriteLine(sum);
---
+++ Day24/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+var input = await File.ReadAllLinesAsync("input.txt");
+var values = input.Where(line => line.Contains(": ")).Select(line => line.Split(": "))
+    .ToDictionary(split => split[0], split => int.Parse(split[1]));
+var gates = input.Where(line => line.Contains("->")).Select(line => line.Split())
+    .ToDictionary(split => split[^1], split => split[..^2]);
+
+Console.WriteLine(long.Parse(
+    string.Join("", gates.Keys.OrderDescending().Where(k => k.StartsWith('z')).Select(GetValue)),
+    NumberStyles.BinaryNumber));
+
+int GetValue(string wire)
+{
+    if (values.TryGetValue(wire, out var output))
+    {
+        return output;
+    }
+
+    var gate = gates[wire];
+
+    var input1 = GetValue(gate[0]);
+    var input2 = GetValue(gate[2]);
+
+    output = gate[1] switch
+    {
+        "AND" => input1 & input2,
+        "OR" => input1 | input2,
+        "XOR" => input1 ^ input2,
+        _ => throw new InvalidOperationException()
+    };
+
+    values[wire] = output;
+
+    return output;
+}
